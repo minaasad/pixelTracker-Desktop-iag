250f20199bb467b844eb529f2c9c3bdd81a1d84c
Mina A. Asad <mina.asad@mina-asad.com>
2014-10-07 09:11:35 +1100

    Initial Commit

---
+++ PixelTrack-Desktop/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace PixelTrack_Desktop
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fILEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.oPENToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nEWIMAGEToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.lblWidthInPixels = new System.Windows.Forms.Label();
+            this.lblHeightInPixels = new System.Windows.Forms.Label();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.lblWidthPhysicalDimension = new System.Windows.Forms.Label();
+            this.lblHeightPhysicalDimension = new System.Windows.Forms.Label();
+            this.groupBox4 = new System.Windows.Forms.GroupBox();
+            this.lblRawFormat = new System.Windows.Forms.Label();
+            this.listColors = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader5 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.toolStripProgressBar1 = new System.Windows.Forms.ToolStripProgressBar();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.statusStrip1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox4.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(6, 19);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(182, 175);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripProgressBar1,
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 539);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(611, 22);
+            this.statusStrip1.TabIndex = 4;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fILEToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(611, 24);
+            this.menuStrip1.TabIndex = 5;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fILEToolStripMenuItem
+            // 
+            this.fILEToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.oPENToolStripMenuItem});
+            this.fILEToolStripMenuItem.Name = "fILEToolStripMenuItem";
+            this.fILEToolStripMenuItem.Size = new System.Drawing.Size(40, 20);
+            this.fILEToolStripMenuItem.Text = "FILE";
+            // 
+            // oPENToolStripMenuItem
+            // 
+            this.oPENToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nEWIMAGEToolStripMenuItem});
+            this.oPENToolStripMenuItem.Name = "oPENToolStripMenuItem";
+            this.oPENToolStripMenuItem.Size = new System.Drawing.Size(103, 22);
+            this.oPENToolStripMenuItem.Text = "Open";
+            // 
+            // nEWIMAGEToolStripMenuItem
+            // 
+            this.nEWIMAGEToolStripMenuItem.Name = "nEWIMAGEToolStripMenuItem";
+            this.nEWIMAGEToolStripMenuItem.Size = new System.Drawing.Size(107, 22);
+            this.nEWIMAGEToolStripMenuItem.Text = "Image";
+            this.nEWIMAGEToolStripMenuItem.Click += new System.EventHandler(this.nEWIMAGEToolStripMenuItem_Click);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(216, 489);
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(104, 45);
+            this.trackBar1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Location = new System.Drawing.Point(406, 334);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(194, 200);
+            this.groupBox1.TabIndex = 7;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Image";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblWidthInPixels);
+            this.groupBox2.Controls.Add(this.lblHeightInPixels);
+            this.groupBox2.Location = new System.Drawing.Point(326, 334);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(74, 69);
+            this.groupBox2.TabIndex = 8;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Size in pixels";
+            // 
+            // lblWidthInPixels
+            // 
+            this.lblWidthInPixels.AutoSize = true;
+            this.lblWidthInPixels.Location = new System.Drawing.Point(6, 50);
+            this.lblWidthInPixels.Name = "lblWidthInPixels";
+            this.lblWidthInPixels.Size = new System.Drawing.Size(21, 13);
+            this.lblWidthInPixels.TabIndex = 9;
+            this.lblWidthInPixels.Text = "W:";
+            // 
+            // lblHeightInPixels
+            // 
+            this.lblHeightInPixels.AutoSize = true;
+            this.lblHeightInPixels.Location = new System.Drawing.Point(6, 37);
+            this.lblHeightInPixels.Name = "lblHeightInPixels";
+            this.lblHeightInPixels.Size = new System.Drawing.Size(18, 13);
+            this.lblHeightInPixels.TabIndex = 0;
+            this.lblHeightInPixels.Text = "H:";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.lblWidthPhysicalDimension);
+            this.groupBox3.Controls.Add(this.lblHeightPhysicalDimension);
+            this.groupBox3.Location = new System.Drawing.Point(326, 409);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(74, 69);
+            this.groupBox3.TabIndex = 9;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Physical Dimension";
+            // 
+            // lblWidthPhysicalDimension
+            // 
+            this.lblWidthPhysicalDimension.AutoSize = true;
+            this.lblWidthPhysicalDimension.Location = new System.Drawing.Point(6, 50);
+            this.lblWidthPhysicalDimension.Name = "lblWidthPhysicalDimension";
+            this.lblWidthPhysicalDimension.Size = new System.Drawing.Size(21, 13);
+            this.lblWidthPhysicalDimension.TabIndex = 9;
+            this.lblWidthPhysicalDimension.Text = "W:";
+            // 
+            // lblHeightPhysicalDimension
+            // 
+            this.lblHeightPhysicalDimension.AutoSize = true;
+            this.lblHeightPhysicalDimension.Location = new System.Drawing.Point(6, 37);
+            this.lblHeightPhysicalDimension.Name = "lblHeightPhysicalDimension";
+            this.lblHeightPhysicalDimension.Size = new System.Drawing.Size(18, 13);
+            this.lblHeightPhysicalDimension.TabIndex = 0;
+            this.lblHeightPhysicalDimension.Text = "H:";
+            // 
+            // groupBox4
+            // 
+            this.groupBox4.Controls.Add(this.lblRawFormat);
+            this.groupBox4.Location = new System.Drawing.Point(326, 484);
+            this.groupBox4.Name = "groupBox4";
+            this.groupBox4.Size = new System.Drawing.Size(74, 50);
+            this.groupBox4.TabIndex = 10;
+            this.groupBox4.TabStop = false;
+            this.groupBox4.Text = "Raw Format";
+            // 
+            // lblRawFormat
+            // 
+            this.lblRawFormat.AutoSize = true;
+            this.lblRawFormat.Location = new System.Drawing.Point(6, 30);
+            this.lblRawFormat.Name = "lblRawFormat";
+            this.lblRawFormat.Size = new System.Drawing.Size(0, 13);
+            this.lblRawFormat.TabIndex = 0;
+            // 
+            // listColors
+            // 
+            this.listColors.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4,
+            this.columnHeader5});
+            this.listColors.Location = new System.Drawing.Point(12, 27);
+            this.listColors.Name = "listColors";
+            this.listColors.Size = new System.Drawing.Size(588, 301);
+            this.listColors.TabIndex = 0;
+            this.listColors.UseCompatibleStateImageBehavior = false;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "ColumnHeadfer";
+            // 
+            // toolStripProgressBar1
+            // 
+            this.toolStripProgressBar1.Name = "toolStripProgressBar1";
+            this.toolStripProgressBar1.Size = new System.Drawing.Size(100, 16);
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(118, 17);
+            this.toolStripStatusLabel1.Text = "toolStripStatusLabel1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 561);
+            this.Controls.Add(this.listColors);
+            this.Controls.Add(this.groupBox4);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox4.ResumeLayout(false);
+            this.groupBox4.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fILEToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem oPENToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nEWIMAGEToolStripMenuItem;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label lblWidthInPixels;
+        private System.Windows.Forms.Label lblHeightInPixels;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label lblWidthPhysicalDimension;
+        private System.Windows.Forms.Label lblHeightPhysicalDimension;
+        private System.Windows.Forms.GroupBox groupBox4;
+        private System.Windows.Forms.Label lblRawFormat;
+        private System.Windows.Forms.ListView listColors;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.ColumnHeader columnHeader5;
+        private System.Windows.Forms.ToolStripProgressBar toolStripProgressBar1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+    }
+}
+
---
+++ PixelTrack-Desktop/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PixelTrack_Desktop
+{
+    public partial class Form1 : Form
+    {
+        private Bitmap workingBitmap { get; set; }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void nEWIMAGEToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            using (OpenFileDialog dlg = new OpenFileDialog())
+            {
+                dlg.Title = "Open Image";
+                dlg.Filter = "Images (*.BMP;*.JPG;*.GIF)|*.BMP;*.JPG;*.GIF";
+
+                if (dlg.ShowDialog() == DialogResult.OK)
+                {
+                    workingBitmap = new Bitmap(dlg.FileName);
+                    displayAll();
+                }
+            }
+        }
+
+        private void displayAll()
+        {
+            
+            displayPicture();
+            displayPhysicalDimensions();
+            displayPixelGroups();
+        }
+
+        private void displayPhysicalDimensions()
+        {
+            float Height = workingBitmap.PhysicalDimension.Height;
+            float Width = workingBitmap.PhysicalDimension.Width;
+
+            lblHeightPhysicalDimension.Text = "H: " + Height;
+            lblWidthPhysicalDimension.Text = "W: " + Width;
+        }
+
+        private void displayPixelGroups()
+        {
+            //display size in pixels
+            int Height = workingBitmap.Size.Height;
+            int Width = workingBitmap.Size.Width;
+
+            lblHeightInPixels.Text = "H: " + Height;
+            lblWidthInPixels.Text = "W: " + Width;
+
+            toolStripProgressBar1.Maximum = Width * Height;
+
+            for (int x = 0; x < Width; x++)
+            {
+                for (int y = 0; y < Height; y++)
+                {
+                    toolStripProgressBar1.Increment(1);
+
+                    Color pixelColor = workingBitmap.GetPixel(x, y);
+
+                    ListViewItem myListViewItem = new ListViewItem(pixelColor.Name);
+                    myListViewItem.BackColor = pixelColor;
+                    myListViewItem.SubItems.Add(x.ToString());
+                    myListViewItem.SubItems.Add(y.ToString());
+                    myListViewItem.SubItems.Add(pixelColor.GetHashCode().ToString());
+
+                    listColors.Items.Add(myListViewItem);
+                }
+            }
+
+        }
+
+        private void displayRawFormat()
+        {
+            lblRawFormat.Text = getReadableRawFormat(workingBitmap.RawFormat);
+        }
+
+        private string getReadableRawFormat(ImageFormat myFormat)
+        {
+            if (myFormat.Equals(System.Drawing.Imaging.ImageFormat.Jpeg))
+                return "Jpeg";
+            if (myFormat.Equals(System.Drawing.Imaging.ImageFormat.Bmp))
+                return "Bmp";
+            if (myFormat.Equals(System.Drawing.Imaging.ImageFormat.Png))
+                return "Png";
+            if (myFormat.Equals(System.Drawing.Imaging.ImageFormat.Gif))
+                return "Gif";
+            else
+                return "Unrecognised";
+        }
+
+        private Hashtable d()
+        {
+            int x = 3, y = 5;
+            Hashtable colorDetails = new Hashtable();
+            Color pixelColor = workingBitmap.GetPixel(x, y);
+
+            colorDetails.Add("Name", pixelColor.Name);
+            colorDetails.Add("X", x);
+            colorDetails.Add("Y", y);
+            colorDetails.Add("AlphaComponentValue", pixelColor.A);
+            colorDetails.Add("BlueComponentValue", pixelColor.B);
+            colorDetails.Add("GreenComponentValue", pixelColor.G);
+            colorDetails.Add("RedComponentValue", pixelColor.R);
+            colorDetails.Add("IsKnownColor", pixelColor.IsKnownColor);
+            colorDetails.Add("IsNamedColor", pixelColor.IsNamedColor);
+            colorDetails.Add("IsSystemColor", pixelColor.IsSystemColor);
+            colorDetails.Add("32bitArgbValue", pixelColor.ToArgb().ToString());
+            colorDetails.Add("KnownColorValue", pixelColor.ToKnownColor().ToString());
+            colorDetails.Add("IsEmpty", pixelColor.IsEmpty);
+            colorDetails.Add("HueSaturationBrightness_SaturationValue", pixelColor.GetSaturation().ToString());
+            colorDetails.Add("HueSaturationBrightness_HueValue", pixelColor.GetHue().ToString());
+            colorDetails.Add("HueSaturationBrightness_BrightnessValue", pixelColor.GetBrightness().ToString());
+            colorDetails.Add("HashCode", pixelColor.GetHashCode().ToString());
+
+            return colorDetails;
+        }
+
+        private void displayPicture()
+        {
+            pictureBox1.Image = workingBitmap ;
+        }
+    }
+}
